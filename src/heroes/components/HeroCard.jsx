bf732998f1c4de7e117b45f06f013fa4cad156f1
juan18506 <juan185067@gmail.com>
2023-09-16 20:56:16 -0300

    deleted assets dir

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -9,7 +9,7 @@ export const HeroCard = ({
   characters,
 }) => {
 
-  const heroImageUrl = `/assets/heroes/${ id }.jpg`;
+  const heroImageUrl = `/heroes/${ id }.jpg`;
 
   return (
     <div className="col">
