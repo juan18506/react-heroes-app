776fa59b9e1fe78f64c330031b80176f0d7e7e0b
juan18506 <juan185067@gmail.com>
2023-09-16 19:05:19 -0300

    created navbar and optimized imports

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,16 +1,18 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import { MarvelPage } from "../heroes/pages/MarvelPage";
-import { DcPage } from "../heroes/pages/DcPage";
-import { LoginPage } from "../auth/pages/LoginPage";
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { MarvelPage, DcPage } from '../heroes';
+import { LoginPage } from '../auth';
+import { Navbar } from '../ui';
 
 export const AppRouter = () => {
   return (
     <>
-      <Routes>
-        <Route path="marvel" element={ <MarvelPage /> } />
-        <Route path="dc" element={ <DcPage /> } />
+      <Navbar />
 
-        <Route path="login" element={ <LoginPage /> } />
+      <Routes>
+        <Route path="/marvel" element={ <MarvelPage /> } />
+        <Route path="/dc" element={ <DcPage /> } />
+
+        <Route path="/login" element={ <LoginPage /> } />
         <Route path="/" element={ <Navigate to="/marvel" /> } />
       </Routes>
     </>
