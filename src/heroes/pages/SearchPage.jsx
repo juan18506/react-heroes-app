b9f906ee1e046aae26d6e6a55267199fbee53fa4
juan18506 <juan185067@gmail.com>
2023-09-17 03:38:56 -0300

    updated classes

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -12,6 +12,8 @@ export const SearchPage = () => {
   const { q = '' } = queryString.parse( location.search );
   const heroes = getHeroesByName( q );
 
+  const showSearch = ( q.length === 0 );
+  const showError = ( q.length > 0 ) && ( heroes.length === 0 );
 
   const { searchText, handleInputChange } = useForm({
     searchText: q,
@@ -19,7 +21,6 @@ export const SearchPage = () => {
 
   const handleSearchSubmit = ( event ) => {
     event.preventDefault();
-    if ( searchText.trim().length <= 1 ) return;
 
     navigate(`?q=${ searchText }`);
   }
@@ -54,11 +55,17 @@ export const SearchPage = () => {
           <h4>Results</h4>
           <hr />
 
-          <div className="alert alert-primary">
+          <div 
+            className="alert alert-primary animate__animated animate__fadeIn" 
+            style={{ display: showSearch ? '' : 'none' }}
+          >
             Search a hero
           </div>
 
-          <div className="alert alert-danger">
+          <div 
+            className="alert alert-danger animate__animated animate__fadeIn" 
+            style={{ display: showError ? '' : 'none' }}
+          >
             No hero with <b>{ q }</b>
           </div>
 
