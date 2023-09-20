a02a3b124c8d2093ab8947863a4b3f09cef31401
juan18506 <juan185067@gmail.com>
2023-09-19 23:05:42 -0300

    Tests in AppRouter

---
+++ tests/router/AppRouter.test.jsx
@@ -0,0 +1,49 @@
+import { describe, test, it, expect, afterEach, vi } from 'vitest';
+import { screen, render, cleanup } from '@testing-library/react';
+import { AppRouter } from '../../src/router';
+import { MemoryRouter } from 'react-router-dom';
+import { AuthContext } from '../../src/auth';
+
+describe('Tests in <AppRouter />', () => {
+
+  afterEach(() => {
+    vi.clearAllMocks();
+    cleanup();
+  });
+
+  it('should show login if not logged', () => {
+    const contextValue = {
+      logged: false,
+    };
+
+    render( 
+      <MemoryRouter initialEntries={['/marvel']}>
+        <AuthContext.Provider value={ contextValue }>
+          <AppRouter />
+        </AuthContext.Provider>
+      </MemoryRouter> 
+    );
+
+    expect( screen.getAllByText('Login').length ).toBeGreaterThan( 0 );
+  });
+
+  it('should show marvel component if not logged', () => {
+    const contextValue = {
+      logged: true,
+      user: {
+        id: '123',
+        name: 'Juan',
+      },
+    };
+
+    render( 
+      <MemoryRouter initialEntries={['/login']}>
+        <AuthContext.Provider value={ contextValue }>
+          <AppRouter />
+        </AuthContext.Provider>
+      </MemoryRouter> 
+    );
+
+    expect( screen.getAllByText('Marvel').length ).toBeGreaterThan( 0 );
+  });
+});
